d58a39d03bde5da3cec3d7cac79bf571b645eca4
Jan1616 <60182539+Jan1616@users.noreply.github.com>
2020-03-12 14:53:29 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,51 @@
 //VOZNJE
-// MISIJA 2(JAJCA NA OKO)
-brick.buttonDown.onEvent(ButtonEvent.Pressed, function () {
-    jaja_na_oko()
-})
-
-// MISIJA 1(ORANŽNA ŽIRAFA)
-brick.buttonUp.onEvent(ButtonEvent.Pressed, function () {
-    oranzna_zirafa()
-})
-
-// MISIJA 4(PUKL)
-brick.buttonLeft.onEvent(ButtonEvent.Pressed, function () {
-    pukl()
+brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
+    if (m == 0) {
+        control.panic(5)
+    }
+    //MISIJA 1(ORANZNA ZIRAFA)
+    if (m == 1) {
+        oranzna_zirafa()
+    }
+    //MISIJA 2(JAJA NA OKO)
+    if (m == 2) {
+        jaja_na_oko()
+    }
+    //MISIJA 3(DREVA)
+    if (m == 3) {
+        dreva()
+    }
+    //MISIJA 4(PUKL)
+    if (m == 4) {
+        pukl()
+    }
 })
 
 // MISIJA 3(MIŠJA PAST)-REZERVA
 //misja_past()
 
-//MISIJA 3(DREVA)
-brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
-    dreva()
+//KALIBRIRA LIGHT
+brick.buttonDown.onEvent(ButtonEvent.Pressed, function () {
+    sensors.color1.reflectedLight()
+    sensors.color2.reflectedLight()
+})
+
+// KALIBRIRA GYRO
+brick.buttonUp.onEvent(ButtonEvent.Pressed, function () {
+    gyro_calibrate()
 })
 
 sensors.color1.calibrateLight(LightIntensityMode.Reflected)
 sensors.color2.calibrateLight(LightIntensityMode.Reflected)
 
-// KALIBRIRA GYRO
 brick.buttonRight.onEvent(ButtonEvent.Pressed, function () {
-    gyro_calibrate()
+    m = m + 1
+    brick.showNumber(m, 1)
+})
+
+brick.buttonLeft.onEvent(ButtonEvent.Pressed, function () {
+    m = m - 1
+    brick.showNumber(m, 1)
 })
 
 //SPREMENLJIVKE
@@ -42,8 +60,7 @@ let svetlost = 0
 let maxmoc = 0
 let smer = 0
 let kot = 0
+let m = 0
 
-sensors.color1.reflectedLight()
-sensors.color2.reflectedLight()
 sensors.gyro3.angle()
 brick.setStatusLight(StatusLight.Orange)
